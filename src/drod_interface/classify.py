1eba71b941d82f10b224d2e42119bfad7b5eee04
Anton Neld <antonherrneld@gmail.com>
2020-12-23 22:10:17 +0100

    Calculate the average color of tiles

--- src/drod_interface/classify.py
+++ src/drod_interface/classify.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy
+
 from common import Entity, ImageProcessingStep
 
 # We will have to revisit this logic to make sure we don't have false positives,
@@ -11,6 +13,10 @@ from common import Entity, ImageProcessingStep
 
 
 def classify_tile(tile, step=None):
+    average_color = numpy.average(tile, (0, 1))
+    if step == ImageProcessingStep.AVERAGE_TILE_COLOR:
+        return [], numpy.ones(tile.shape) * average_color
+
     entities = [Entity.UNKNOWN]
     if step == ImageProcessingStep.CLASSIFY_TILES:
         modified_tile = tile.copy()
