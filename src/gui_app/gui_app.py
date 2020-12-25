2a44ba0874f4534e04978d9f6dee20326f073e47
Anton Neld <antonherrneld@gmail.com>
2020-12-25 21:25:09 +0100

    Add button to toggle view size
    Some processing involves single-pixel lines, which are sometimes
    not visible at smaller sizes.

--- src/gui_app/gui_app.py
+++ src/gui_app/gui_app.py
@@ -11,6 +11,8 @@ QUEUE_POLL_INTERVAL = 50
 # The DROD room size is 836x704, use half that for canvas to preserve aspect ratio
 CANVAS_WIDTH = 418
 CANVAS_HEIGHT = 352
+LARGE_CANVAS_WIDTH = 836
+LARGE_CANVAS_HEIGHT = 704
 
 
 class GuiApp(tkinter.Frame):
@@ -24,6 +26,8 @@ class GuiApp(tkinter.Frame):
         self.selected_view_step.set(list(ImageProcessingStep)[-1].value)
         self.selected_strategy = tkinter.StringVar(self)
         self.selected_strategy.set(list(Strategy)[0].value)
+        self.enlarged_view = False
+        self.raw_view_image = None
         self.pack()
         self.create_widgets()
         self.root.after(QUEUE_POLL_INTERVAL, self.check_queue)
@@ -37,6 +41,10 @@ class GuiApp(tkinter.Frame):
         self.control_panel.pack(side=tkinter.RIGHT)
         self.view_controls = tkinter.Frame(self.control_panel)
         self.view_controls.pack(side=tkinter.TOP)
+        self.toggle_view_size_button = tkinter.Button(
+            self.view_controls, text="Enlarge view", command=self.toggle_view_size
+        )
+        self.toggle_view_size_button.pack(side=tkinter.RIGHT)
         self.get_view = tkinter.Button(
             self.view_controls, text="Get view", command=self.show_view
         )
@@ -68,15 +76,20 @@ class GuiApp(tkinter.Frame):
             if item == GUIEvent.QUIT:
                 self.root.destroy()
             elif item == GUIEvent.DISPLAY_IMAGE:
-                resized_image = detail.resize(
-                    (CANVAS_WIDTH, CANVAS_HEIGHT), Image.LANCZOS
-                )
-                # Assign to self.view to prevent from being garbage collected
-                self.view = ImageTk.PhotoImage(image=resized_image)
-                self.canvas.create_image(0, 0, image=self.view, anchor=tkinter.NW)
+                self.raw_view_image = detail
+                self.draw_view()
         except Empty:
             pass
         self.root.after(QUEUE_POLL_INTERVAL, self.check_queue)
+
+    def draw_view(self):
+        if self.raw_view_image is not None:
+            resized_image = self.raw_view_image.resize(
+                (int(self.canvas["width"]), int(self.canvas["height"])), Image.LANCZOS
+            )
+            # Assign to self.view to prevent from being garbage collected
+            self.view = ImageTk.PhotoImage(image=resized_image)
+            self.canvas.create_image(0, 0, image=self.view, anchor=tkinter.NW)
 
     def run_coroutine(self, coroutine):
         async def wrapped_coroutine():
@@ -96,3 +109,15 @@ class GuiApp(tkinter.Frame):
         step_value = self.selected_view_step.get()
         step = next(e for e in ImageProcessingStep if e.value == step_value)
         self.run_coroutine(self.bot.show_view(step))
+
+    def toggle_view_size(self):
+        if self.enlarged_view:
+            self.enlarged_view = False
+            self.canvas.configure(height=CANVAS_HEIGHT, width=CANVAS_WIDTH)
+            self.toggle_view_size_button.configure(text="Enlarge view")
+            self.draw_view()
+        else:
+            self.enlarged_view = True
+            self.canvas.configure(height=LARGE_CANVAS_HEIGHT, width=LARGE_CANVAS_WIDTH)
+            self.toggle_view_size_button.configure(text="Ensmall view")
+            self.draw_view()
