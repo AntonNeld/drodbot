a4543578a82677ff2f795cf1a5da8191bb7fa105
Anton Neld <antonherrneld@gmail.com>
2020-12-29 14:12:46 +0100

    Capture triggered conquer tokens in training data

--- src/classification_trainer/classification_trainer.py
+++ src/classification_trainer/classification_trainer.py
@@ -23,6 +23,7 @@ class ClassificationTrainer:
 
         Work in progress.
         """
+        print("Generating training data")
         await self._interface.initialize(editor=True)
         await self._interface.editor_clear_room()
         room = Room()
@@ -40,13 +41,13 @@ class ClassificationTrainer:
         )
         room.place_element_like_editor(Element.BEETHRO, Direction.NE, (11, 20))
 
-        await self._interface.editor_start_test_room((37, 31), Direction.SE)
+        # Starting position when testing
         room.place_element_like_editor(Element.BEETHRO, Direction.SE, (37, 31))
 
+        await self._interface.editor_start_test_room((37, 31), Direction.SE)
         visual_info = await self._interface.get_view(
             step=ImageProcessingStep.EXTRACT_TILES
         )
-
         await self._interface.editor_stop_test_room()
 
         if not os.path.exists(self._training_data_dir):
@@ -56,17 +57,41 @@ class ClassificationTrainer:
         )
         for coords, tile in visual_info["tiles"].items():
             # Annotate the image with the tile contents
-            # Temporary tile contents for now.
             tile_info = room.get_tile(coords)
-            png_info = PngInfo()
-            png_info.add_text("tile_json", tile_info.to_json())
-            # Save the image with a random name
-            image = PIL.Image.fromarray(tile)
-            image.save(
-                os.path.join(
-                    self._training_data_dir,
-                    f"{random_string}_{coords[0]}_{coords[1]}.png",
-                ),
-                "PNG",
-                pnginfo=png_info,
+            _save_tile_png(
+                coords, tile, tile_info, random_string, self._training_data_dir
             )
+        # Place a conquer token under the starting position and go again,
+        # to capture triggered conquer tokens in the screenshot
+        await self._interface.editor_place_element(
+            Element.CONQUER_TOKEN, Direction.NONE, (37, 31)
+        )
+        room.place_element_like_editor(Element.CONQUER_TOKEN, Direction.NONE, (37, 31))
+        await self._interface.editor_start_test_room((37, 31), Direction.SE)
+        visual_info = await self._interface.get_view(
+            step=ImageProcessingStep.EXTRACT_TILES
+        )
+        await self._interface.editor_stop_test_room()
+        for coords in room.find_coordinates(Element.CONQUER_TOKEN):
+            tile = visual_info["tiles"][coords]
+            tile_info = room.get_tile(coords)
+            tile_info.item = (Element.TRIGGERED_CONQUER_TOKEN, Direction.NONE)
+            _save_tile_png(
+                coords, tile, tile_info, f"{random_string}v", self._training_data_dir
+            )
+        print("Finished generating training data")
+
+
+def _save_tile_png(coords, tile, tile_info, base_name, directory):
+    png_info = PngInfo()
+    png_info.add_text("tile_json", tile_info.to_json())
+    # Save the image with a random name
+    image = PIL.Image.fromarray(tile)
+    image.save(
+        os.path.join(
+            directory,
+            f"{base_name}_{coords[0]}_{coords[1]}.png",
+        ),
+        "PNG",
+        pnginfo=png_info,
+    )
