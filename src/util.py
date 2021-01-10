3ec9956aae5aa2e0d674926c7528f9f06c65f757
Anton Neld <antonherrneld@gmail.com>
2021-01-10 14:35:58 +0100

    Document util.py

--- src/util.py
+++ src/util.py
@@ -2,6 +2,20 @@ import numpy
 
 
 def find_color(image, color):
+    """Find all pixels with a specific color.
+
+    Parameters
+    ----------
+    image
+        The image to find the color in.
+    color
+        The color to find.
+
+    Returns
+    -------
+    A boolean 2D numpy array, which is True where the image
+    has the color and False elsewhere.
+    """
     r = image[:, :, 0]
     g = image[:, :, 1]
     b = image[:, :, 2]
