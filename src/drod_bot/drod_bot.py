239449c8a37e3886a64fb7716e003f327e1d73ce
Anton Neld <antonherrneld@gmail.com>
2021-01-10 14:18:39 +0100

    Document DrodBot

--- src/drod_bot/drod_bot.py
+++ src/drod_bot/drod_bot.py
@@ -4,30 +4,47 @@ import random
 from common import Action, Strategy, Element
 from .pathfinding import find_path
 
-ACTION_DELAY = 0.1
+_ACTION_DELAY = 0.1
 
 
 class DrodBot:
+    """This class if responsible for playing DROD.
+
+    Parameters
+    ----------
+    drod_interface
+        The interface for playing rooms.
+    window_queue
+        A queue for sending updates to the GUI.
+    """
+
     def __init__(self, drod_interface, window_queue):
         self._interface = drod_interface
         self._queue = window_queue
 
     async def run_strategy(self, strategy):
+        """Have Beethro do something, usually trying to solve the room.
+
+        Parameters
+        ----------
+        strategy
+            The strategy to execute.
+        """
         await self._interface.initialize()
         visual_info = await self._interface.get_view()
         room = visual_info["room"]
         if strategy == Strategy.MOVE_RANDOMLY:
             actions = random.choices(list(Action), k=30)
-            await self.do_actions(actions)
+            await self._do_actions(actions)
         elif strategy == Strategy.MOVE_TO_CONQUER_TOKEN:
             player_position = room.find_player()
             conquer_positions = room.find_coordinates(Element.CONQUER_TOKEN)
             actions = find_path(player_position, conquer_positions, room)
-            await self.do_actions(actions)
+            await self._do_actions(actions)
         else:
             raise RuntimeError(f"Unknown strategy {strategy}")
 
-    async def do_actions(self, actions):
+    async def _do_actions(self, actions):
         for action in actions:
             await self._interface.do_action(action)
-            await asyncio.sleep(ACTION_DELAY)
+            await asyncio.sleep(_ACTION_DELAY)
