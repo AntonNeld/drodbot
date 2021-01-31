70dbb33c898400027f40bee476c30eb87ba306a4
Anton Neld <antonherrneld@gmail.com>
2021-01-31 15:07:26 +0100

    Add commented out import from DRODLib
    It does not compile yet, which is why it is commented out.

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 # Run this to compile the C++ parts, like DRODLib.
 # It may be replaced by something more standard later.
 g++ -O3 -Wall -Werror -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` \
+-Isrc/drod_lib/drod \
 src/drod_lib/temp.cpp -o src/drod_lib/temp_module_name`python3.8-config --extension-suffix`
