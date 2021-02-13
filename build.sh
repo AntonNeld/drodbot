a00ef8df62cba29687d13ddba029a078f5626876
Anton Neld <antonherrneld@gmail.com>
2021-02-13 15:43:51 +0100

    Generate stub for room_simulator
    This is to get intellisense for it.

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ if [ ! -d build ]; then
     pipenv run make clean
 fi
 pipenv run make
+PYTHONPATH=./ pipenv run pybind11-stubgen room_simulator
+mv stubs/room_simulator-stubs/__init__.pyi room_simulator.pyi
+rm -r stubs
