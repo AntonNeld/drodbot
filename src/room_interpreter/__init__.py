488f2aefa3888f18fec7bd632cc01dcdd0b41232
Anton Neld <antonherrneld@gmail.com>
2021-03-19 21:29:42 +0100

    Create RoomInterpreter

--- src/room_interpreter/__init__.py
+++ src/room_interpreter/__init__.py
@@ -1,3 +1,4 @@
 from .room_conversion import room_from_apparent_tiles
+from .room_interpreter import RoomInterpreter
 
-__all__ = ("room_from_apparent_tiles",)
+__all__ = ("room_from_apparent_tiles", "RoomInterpreter")
---
+++ src/room_interpreter/room_interpreter.py
@@ -0,0 +1,42 @@
+from .room_conversion import room_from_apparent_tiles
+
+
+class RoomInterpreter:
+    """This is used to keep track of the possible contents of a room.
+
+    Parameters
+    ----------
+    classifier
+        A tile classifier.
+    """
+
+    def __init__(self, classifier, play_interface):
+        self._classifier = classifier
+        self._interface = play_interface
+
+    def get_initial_room(self, return_debug_images=False):
+        """Get an initial guess of a room by taking a screenshot.
+
+        Parameters
+        ----------
+        return_debug_images
+            If True, return an additional list of tuples (name, debug_image).
+
+        Returns
+        -------
+        room
+            A room.
+        debug_images
+            Only returned if `return_debug_images` is True. A list of (name, image).
+        """
+        if return_debug_images:
+            tile_contents, orb_effects, debug_images = self._interface.get_view(
+                return_debug_images=True
+            )
+        else:
+            tile_contents, orb_effects = self._interface.get_view()
+        room = room_from_apparent_tiles(tile_contents, orb_effects)
+
+        if return_debug_images:
+            return room, debug_images
+        return room
