b43c8dc79ad6c549c70a88fca2f3537608859d26
Anton Neld <antonherrneld@gmail.com>
2021-04-11 17:02:35 +0200

    Add separate directory for problems
    They will be exported directly.

---
+++ src/room_simulator/problems/RoomProblem.h
@@ -0,0 +1,19 @@
+#include "../Room.h"
+#include "../typedefs.h"
+#include "../search/Problem.h"
+#include "../RoomPlayer.h"
+
+class RoomProblem : public Problem<Room, Action>
+{
+public:
+    RoomProblem(Room room, Objective objective);
+    Room initialState();
+    std::set<Action> actions(Room state);
+    Room result(Room state, Action action);
+    bool goalTest(Room state);
+    int stepCost(Room state, Action action, Room result);
+
+private:
+    Room room;
+    Objective objective;
+};
