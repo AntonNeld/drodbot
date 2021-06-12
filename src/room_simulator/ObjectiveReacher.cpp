790e61d5491af0aaace93b55b713aed57ecf0004
Anton Neld <antonherrneld@gmail.com>
2021-06-12 12:00:11 +0200

    Make ObjectiveReacher inspectable

--- src/room_simulator/ObjectiveReacher.cpp
+++ src/room_simulator/ObjectiveReacher.cpp
@@ -1,27 +1,102 @@
 #include <vector>
 #include <map>
 #include <tuple>
+#include <optional>
 #include "ObjectiveReacher.h"
 #include "Objective.h"
 #include "typedefs.h"
 #include "search/Searcher.h"
 #include "problems/RoomProblem.h"
 
-ObjectiveReacher::ObjectiveReacher() : cachedSolutions({}){};
+ObjectiveReacher::ObjectiveReacher() : cachedSolutions({}),
+                                       phase(ObjectiveReacherPhase::NOTHING),
+                                       currentRoom(std::nullopt),
+                                       currentObjective(std::nullopt),
+                                       solution(std::nullopt),
+                                       roomProblem(std::nullopt),
+                                       simulationSearcher(std::nullopt){};
+
+ObjectiveReacher::~ObjectiveReacher()
+{
+    if (this->roomProblem)
+    {
+        delete this->roomProblem.value();
+    }
+};
 
 Solution<Room, Action> ObjectiveReacher::findSolution(Room room, Objective objective)
 {
-    // Return solution from the cache if it exists
-    auto foundSolution = this->cachedSolutions.find({room, objective});
-    if (foundSolution != this->cachedSolutions.end())
+    this->start(room, objective);
+    while (this->phase != ObjectiveReacherPhase::FINISHED)
     {
-        return std::get<1>(*foundSolution);
+        this->nextPhase();
     }
-    // Else, try to find the solution
-    RoomProblem problem = RoomProblem(room, objective);
-    // Low iteration limit for now, to avoid finding the solution indirectly by accident
-    Searcher<Room, Action> searcher = Searcher<Room, Action>(&problem, true, true, false, 100);
-    Solution<Room, Action> solution = searcher.findSolution();
-    this->cachedSolutions.insert({{room, objective}, solution});
-    return solution;
+    this->phase = ObjectiveReacherPhase::NOTHING;
+    return this->solution.value();
 };
+
+void ObjectiveReacher::start(Room room, Objective objective)
+{
+    this->currentRoom = room;
+    this->currentObjective = objective;
+    this->solution = std::nullopt;
+    this->roomProblem = std::nullopt;
+    this->simulationSearcher = std::nullopt;
+    this->phase = ObjectiveReacherPhase::CHECK_CACHE;
+}
+
+void ObjectiveReacher::nextPhase()
+{
+    switch (this->phase)
+    {
+    case ObjectiveReacherPhase::NOTHING:
+        break; // Do nothing
+    case ObjectiveReacherPhase::CHECK_CACHE:
+    {
+        auto foundSolutionPtr = this->cachedSolutions.find({this->currentRoom.value(), this->currentObjective.value()});
+        if (foundSolutionPtr != this->cachedSolutions.end())
+        {
+            this->solution = std::get<1>(*foundSolutionPtr);
+            this->phase = ObjectiveReacherPhase::FINISHED;
+        }
+        else
+        {
+            if (this->roomProblem)
+            {
+                delete this->roomProblem.value();
+            }
+            this->roomProblem = new RoomProblem(this->currentRoom.value(), this->currentObjective.value());
+            // Low iteration limit for now, to avoid finding the solution indirectly by accident
+            this->simulationSearcher = Searcher<Room, Action>(this->roomProblem.value(), true, true, false, 100);
+            this->phase = ObjectiveReacherPhase::SIMULATE_ROOM;
+        }
+        break;
+    }
+    case ObjectiveReacherPhase::SIMULATE_ROOM:
+    {
+        this->solution = this->simulationSearcher.value().findSolution();
+        this->cachedSolutions.insert({{this->currentRoom.value(), this->currentObjective.value()}, this->solution.value()});
+        this->phase = ObjectiveReacherPhase::FINISHED;
+        break;
+    }
+    case ObjectiveReacherPhase::FINISHED:
+        break; // Do nothing
+    default:
+        throw std::invalid_argument("Unknown phase");
+    }
+}
+
+ObjectiveReacherPhase ObjectiveReacher::getPhase()
+{
+    return this->phase;
+}
+
+Solution<Room, Action> ObjectiveReacher::getSolution()
+{
+    return this->solution.value();
+}
+
+Searcher<Room, Action> ObjectiveReacher::getRoomSimulationSearcher()
+{
+    return this->simulationSearcher.value();
+}
--- src/room_simulator/ObjectiveReacher.h
+++ src/room_simulator/ObjectiveReacher.h
@@ -4,18 +4,41 @@
 #include <tuple>
 #include <vector>
 #include <map>
+#include <optional>
 #include "typedefs.h"
 #include "Room.h"
 #include "Objective.h"
+#include "problems/RoomProblem.h"
+#include "search/Searcher.h"
+
+enum class ObjectiveReacherPhase
+{
+    NOTHING,
+    CHECK_CACHE,
+    SIMULATE_ROOM,
+    FINISHED,
+};
 
 class ObjectiveReacher
 {
 public:
     ObjectiveReacher();
+    ~ObjectiveReacher();
     Solution<Room, Action> findSolution(Room room, Objective objective);
+    void start(Room room, Objective objective);
+    void nextPhase();
+    ObjectiveReacherPhase getPhase();
+    Solution<Room, Action> getSolution();
+    Searcher<Room, Action> getRoomSimulationSearcher();
 
 private:
     std::map<std::tuple<Room, Objective>, Solution<Room, Action>> cachedSolutions;
+    ObjectiveReacherPhase phase;
+    std::optional<Room> currentRoom;
+    std::optional<Objective> currentObjective;
+    std::optional<Solution<Room, Action>> solution;
+    std::optional<RoomProblem *> roomProblem;
+    std::optional<Searcher<Room, Action>> simulationSearcher;
 };
 
 #endif // DRODBOT_OBJECTIVEREACHER_H
